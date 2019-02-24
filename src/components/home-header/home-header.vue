d9f52cbb8fa5bea4bef0b2d50b123936141262c8
andefine <1939866551@qq.com>
2019-02-24 23:36:36 +0800

    首页header

---
+++ src/components/home-header/home-header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <span class="iconfont icon-list"></span>
+    <div class="middle">
+      <span class="iconfont icon-music"></span>
+      <span class="iconfont icon-netease"></span>
+      <span class="iconfont icon-video"></span>
+    </div>
+    <span class="iconfont icon-search"></span>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 42px;
+  height: 148px;
+  background: $theme;
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="home">
-    <div class="header">
-      <div class="menu iconfont icon-list"></div>
-    </div>
+    <home-header/>
   </div>
 </template>
 
 <script>
+import HomeHeader from '@/components/home-header/home-header.vue'
 export default {
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
 <style lang="scss">
-.header {
-  height: 148px;
-  background: $theme;
-}
+
 </style>
