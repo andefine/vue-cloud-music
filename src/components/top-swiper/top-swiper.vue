d99ece2b43aed77f0dfa0df354f91e14dbd57bba
andefine <1939866551@qq.com>
2019-03-09 21:05:26 +0800

    使用vue-awesome-swiper完成头部banner轮播图

---
+++ src/components/top-swiper/top-swiper.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="top-swiper">
+    <div class="top-swiper__bg"></div>
+    <swiper class="swiper-com" :options="swiperOption" ref="topSwiper">
+      <swiper-slide class="swiper-slide" v-for="(item, index) in data" :key="index">
+        <img class="swiper-slide__img" :src="item.imageUrl" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.css'
+
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data () {
+    return {
+      swiperOption: {
+        loop: true,
+        autoplay: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          // eslint-disable-next-line
+          renderBullet(index, className) {
+            return `<span class="${className} swiper-pagination-bullet-custom"></span>`
+          }
+        }
+      }
+    }
+  },
+  props: {
+    data: Array
+  }
+}
+</script>
+
+<style lang="scss">
+.top-swiper {
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 442px;
+  width: 100%;
+  &__bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 306px;
+    background: $theme;
+  }
+}
+
+.swiper-com {
+  width: 100%;
+  height: 406px;
+}
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  &__img {
+    width: 1044px;
+    height: 100%;
+    border-radius: 16px;
+  }
+}
+.swiper-pagination-bullet-custom {
+  margin: 0 11px !important;
+  width: 22px;
+  height: 22px;
+  border-radius: 50%;
+  background: #fff;
+  opacity: 0.4;
+  &.swiper-pagination-bullet-active {
+    background: $theme;
+    opacity: 0.9;
+  }
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,17 +2,14 @@
   <div class="home">
     <home-header/>
     <home-nav/>
-    <div class="home-swiper">
-      <div class="home-swiper__bg"></div>
-      <base-swiper :data="banners" />
-    </div>
+    <top-swiper :data="banners" />
   </div>
 </template>
 
 <script>
 import HomeHeader from './home-header'
 import HomeNav from './home-nav'
-import BaseSwiper from '@/components/base-swiper/base-swiper'
+import TopSwiper from '@/components/top-swiper/top-swiper'
 
 import * as api from '@/api'
 
@@ -25,25 +22,18 @@ export default {
   components: {
     HomeHeader,
     HomeNav,
-    BaseSwiper
+    TopSwiper
   },
   async created () {
     const { banners } = await api.getBanners()
     this.banners = banners
+  },
+  methods: {
+
   }
 }
 </script>
 
 <style lang="scss">
-.home-swiper {
-  position: relative;
-  &__bg {
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 324px;
-    background: $theme;
-  }
-}
+
 </style>
