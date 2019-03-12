659dc7e9edade22cf34b28066200901bca1075cc
andefine <1939866551@qq.com>
2019-03-12 20:42:37 +0800

    首页推荐歌单，刷新后都不一样，随机的 6 条

--- src/views/home/recommend-playlist.vue
+++ src/views/home/recommend-playlist.vue
@@ -24,10 +24,26 @@ export default {
     }
   },
   async created () {
-    const { playlists } = await api.getPlaylist({
-      order: 'hot'
-    })
-    this.playlists = playlists
+    await this.pickPlaylistsRandomly()
+  },
+  methods: {
+    // 因为首页的推荐歌单每次刷新过后可能都不一样，但是呢，似乎并没有这个接口提供。那么，我们就模拟一个随机吧，先获取到 100 条，从中随机挑选 6 条
+    async pickPlaylistsRandomly () {
+      const { playlists } = await api.getPlaylist({
+        order: 'hot'
+      })
+
+      // 随机算法 [javascript如何在一个数组里随机选出不重复的10项?](https://segmentfault.com/q/1010000006819233) 的第二条回答，效率更高哟
+      const len = playlists.length
+      for (let i = 0; i < 6; i++) {
+        const range = len - i
+        const rand = i + Math.floor(Math.random() * range)
+        const temp = playlists[i]
+        playlists[i] = playlists[rand]
+        playlists[rand] = temp
+      }
+      this.playlists = playlists.splice(0, 6)
+    }
   }
 }
 </script>
